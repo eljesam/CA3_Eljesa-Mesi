9a012399f6b1785cea8bc1701d505f534d15e336
eljesam <D00251881@student.dkit.ie>
2024-02-02 09:09:46 +0000

    completed question 6

--- CA3_Eljesa-Mesi/CA3_Eljesa-Mesi/src/Block.java
+++ CA3_Eljesa-Mesi/CA3_Eljesa-Mesi/src/Block.java
@@ -1,5 +1,35 @@
 public class Block {
     int qty;
     int price;
+    double cost;
 
+    public Block(int qty, int price) {
+        this.qty = qty;
+        this.price = price;
+    }
+
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public double getCost() {
+        return price * qty;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
 }
--- CA3_Eljesa-Mesi/CA3_Eljesa-Mesi/src/CA3_Question6.java
+++ CA3_Eljesa-Mesi/CA3_Eljesa-Mesi/src/CA3_Question6.java
@@ -14,15 +14,29 @@ public class CA3_Question6
     or
     sell qty price
     or
-    quit
-     */
-
-
+    quit */
 
     public static void main(String[] args) {
-
-         Scanner in = new Scanner(System.in);
-        String command="";
+        //create queue for the blocks
+        Queue<Block> blocks = new LinkedList<>();
+        //create scanner
+        Scanner in = new Scanner(System.in);
+        //create command string
+        String command;
+        //create block object
+        Block block;
+        //create share object
+        Share share;
+        //create total gain variable
+        double totalGain = 0;
+        //create total cost variable
+        double totalCost = 0;
+        //create total shares variable
+        int totalShares = 0;
+        //create total shares sold variable
+        int totalSharesSold = 0;
+        //create total shares bought variable
+        int totalSharesBought = 0;
             do {
             System.out.print(">");
             command = in.next();
@@ -30,17 +44,50 @@ public class CA3_Question6
             {
                 int qty = in.nextInt();
                 double price = in.nextDouble();
+                block = new Block(qty, (int) price);
+                blocks.add(block);
+                totalSharesBought += qty;
+                totalShares += qty;
+                totalCost += block.getCost();
+
 
             }
             else if(command.equals("sell"))
             {
                 int qty = in.nextInt();
                 double price = in.nextDouble();
+                double gain = 0;
+                int sharesSold = 0;
+                int sharesBought = 0;
+                while(qty > 0)
+                {
+                    block = blocks.peek();
+                    if(block.getQty() <= qty)
+                    {
+                        qty -= block.getQty();
+                        gain += (price - block.getPrice()) * block.getQty();
+                        sharesSold += block.getQty();
+                        blocks.remove();
+                    }
+                    else
+                    {
+                        gain += (price - block.getPrice()) * qty;
+                        sharesSold += qty;
+                        block.setQty(block.getQty() - qty);
+                        qty = 0;
+                    }
+                }
 
 
             }
 
         }while(!command.equalsIgnoreCase("quit"));
+        System.out.println("Total shares: " + totalShares);
+        System.out.println("Total shares bought: " + totalSharesBought);
+        System.out.println("Total shares sold: " + totalSharesSold);
+        System.out.println("Total cost: " + totalCost);
+        System.out.println("Total gain: " + totalGain);
+
 
     }
 }
