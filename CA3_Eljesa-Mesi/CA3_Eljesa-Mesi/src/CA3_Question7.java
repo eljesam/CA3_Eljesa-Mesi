5db94821a2425a9a9f9d0ffeac361332150cfd68
eljesam <D00251881@student.dkit.ie>
2024-02-02 09:20:01 +0000

    completed question 7

--- CA3_Eljesa-Mesi/CA3_Eljesa-Mesi/src/CA3_Question7.java
+++ CA3_Eljesa-Mesi/CA3_Eljesa-Mesi/src/CA3_Question7.java
@@ -1,7 +1,7 @@
 import java.util.*;
 /**
- *  Name:
- *  Class Group:
+ *  Name: Eljesa Mesi
+ *  Class Group: SD2B
  */
 public class CA3_Question7
 {
@@ -13,7 +13,10 @@ public class CA3_Question7
    or
    quit
     */
+    //Hint: Keep a Map<String, Queue<Block>> to store the shares of each company.
     public static void main(String[] args) {
+        //create map for the blocks
+        Map<String, Queue<Block>> blocks = new HashMap<>();
 
         Scanner in = new Scanner(System.in);
         String command="";
@@ -26,6 +29,17 @@ public class CA3_Question7
                 int qty = in.nextInt();
                 double price = in.nextDouble();
                 // Code to buy shares here
+                Block block = new Block(qty, (int) price);
+                if(blocks.containsKey(company))
+                {
+                    blocks.get(company).add(block);
+                }
+                else
+                {
+                    Queue<Block> queue = new LinkedList<>();
+                    queue.add(block);
+                    blocks.put(company, queue);
+                }
             }
             else if(command.equals("sell"))
             {
@@ -33,7 +47,22 @@ public class CA3_Question7
                 int qty = in.nextInt();
                 double price = in.nextDouble();
                 // Code to sell shares and calculate profit here
+                if(blocks.containsKey(company))
+                {
+                    Queue<Block> queue = blocks.get(company);
+                    while(qty > 0 && !queue.isEmpty()) {
+                        Block block = queue.peek();
+                        if (block.getQty() <= qty) {
+                            qty -= block.getQty();
+                            queue.poll();
+                        } else {
+                            block.setQty(block.getQty() - qty);
+                            qty = 0;
+                        }
+                    }
+                }
             }
         }while(!command.equalsIgnoreCase("quit"));
+
     }
 }
